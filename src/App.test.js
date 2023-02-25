58624b32b05ae039c9fa531776505f7e8a83feeb
Julio Gaiotto <juliogaiotto@MacBook-Air-de-Julio.local>
2023-02-25 14:15:20 -0300

    feat: removendo arquivos desnecessários da estrutura do projeto

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders ola', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ola/i);
   expect(linkElement).toBeInTheDocument();
 });
